7da6a3e1b7139deb8f0e4656cb67adace10e1694
aylamalek <85609827+aylamalek@users.noreply.github.com>
2021-06-09 09:45:25 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var getUserName = prompt("Hello! Who are you? Please sign in:")
+
+console. log(getUserName) ; 
+
+document.getElementById("ayla").innerText= getUserName; 
+
+// tooltip 
+
+/* tooltip js */ 
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
